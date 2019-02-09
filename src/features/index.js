0e2cde4f5cb6af0e1ca77b7b358c54abad711e55
Derrick <derrick.xiang@gmail.com>
2019-02-09 12:17:14 +0800

    initial commit

---
+++ src/features/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './app/layout/App';
+import * as serviceWorker from './serviceWorker';
+
+const rootEL = document.getElementById('root');
+
+let render = () => {
+    ReactDOM.render(<App />, rootEL)
+}
+
+if (module.hot) {
+    module.hot.accept('./app/layout/App', () => {
+        setTimeout(render)
+    })
+}
+
+render();
+
+serviceWorker.register();
